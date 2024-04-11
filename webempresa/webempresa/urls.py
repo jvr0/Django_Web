e1db605cf31a0a07342766e1fc05831c406e5bf6
Joaquín Villaverde <joaquin.villaverde@thekeytalent.com>
2024-04-11 14:42:34 +0200

    inicio webempresa

---
+++ webempresa/webempresa/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    # Paths del Core
+    path('', include('core.urls')),
+    # Paths del Admin
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    from django. conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

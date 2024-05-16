03ff644d81da227f1475e1982e1c6f91f47d3105
Joaquin Villaverde Roldan <joaquin@MacBook-Air-de-Joaquin.local>
2024-05-16 23:10:28 +0200

    avanzamos en el curso

---
+++ webplayground/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Page
+
+class PageListView(ListView):
+    model = Page
+    
+class PageDetailView(DetailView):
+    model = Page
